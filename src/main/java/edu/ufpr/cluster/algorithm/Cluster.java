03bb360e2e43fd3123ab723ab94a7b09a9e06c97
Vidal Fontoura <vfontoura@kenzanmedia.com>
2015-08-31 12:20:31 -0300

    Adding cluster addPoint and update centroid methods

--- src/main/java/edu/ufpr/cluster/algorithm/Cluster.java
+++ src/main/java/edu/ufpr/cluster/algorithm/Cluster.java
@@ -3,6 +3,8 @@ package edu.ufpr.cluster.algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.ufpr.cluster.algorithms.functions.DistanceFunction;
+
 public class Cluster {
 
 	
@@ -32,6 +34,32 @@ public class Cluster {
 
 	public void setPoints(List<Point> points) {
 		this.points = points;
+	}
+	
+	public void addPoint(Point point) {
+		point.setCluster(this);
+		getPoints().add(point);
+	}
+	
+	public void updateCentroid() {
+		
+		int dimension = centroid.getCoordinates().size();
+		double[] sumCoordinates = new double[dimension];
+		for(int i=0; i<points.size(); i++) {
+			
+			List<Double> coordinates = points.get(i).getCoordinates();
+			for(int j=0; j<coordinates.size(); j++) {
+				Double coordinate = coordinates.get(j);
+				sumCoordinates[j] = sumCoordinates[j] + coordinate;
+			}
+			
+		}
+		
+		for(int i=0; i<sumCoordinates.length; i++) {
+			double mean = sumCoordinates[i]/dimension;
+			centroid.getCoordinates().set(i, mean);
+		}
+		
 	}
 	
 	
